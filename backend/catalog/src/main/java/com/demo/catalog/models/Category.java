80431a9c1a3e3980d6bf1e14595af7539c1633c6
Alberto Nascimento <beto.bdn@gmail.com>
2021-11-13 20:34:24 -0300

    Post Category

--- backend/catalog/src/main/java/com/demo/catalog/models/Category.java
+++ backend/catalog/src/main/java/com/demo/catalog/models/Category.java
@@ -34,6 +34,10 @@ public class Category implements Serializable {
 	public String getName() {
 		return name;
 	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	@Override
 	public int hashCode() {
