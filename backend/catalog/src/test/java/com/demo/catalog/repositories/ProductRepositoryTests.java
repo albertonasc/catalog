7c4a5829ed9dbe456a6c25b25c464e9be8d31ba8
Alberto Nascimento <beto.bdn@gmail.com>
2021-11-16 15:05:59 -0300

    Add repository tests

---
+++ backend/catalog/src/test/java/com/demo/catalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,71 @@
+package com.demo.catalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.demo.catalog.models.Product;
+import com.demo.catalog.tests.Factory;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	private long existingId;
+	private long nonExistingId;
+	private long countTotalProducts;
+	
+	@BeforeEach
+	void setUp() {
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void saveShouldPersistWithAutoincrementWhenidIsNull() {
+		
+		Product product = Factory.createProduct();
+		product.setId(null);
+		
+		product = repository.save(product);
+		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
+	}
+	
+	@Test
+	public void findByIdShouldReturnNonEmptyOptionalWhenIdExists() {
+		Optional<Product> result = repository.findById(existingId);
+		Assertions.assertTrue(result.isPresent());
+	}
+	
+	@Test
+	public void findByIdShouldReturnEmptyOptionalWhenIdDoesNotExist() {
+		Optional<Product> result = repository.findById(nonExistingId);
+		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {
+		repository.deleteById(existingId);
+		
+		Optional<Product> result = repository.findById(existingId);
+		Assertions.assertFalse(result.isPresent());
+	}
+	
+	@Test 
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {	
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);
+		});
+	}
+			
+}
---
+++ backend/catalog/src/test/java/com/demo/catalog/tests/Factory.java
@@ -0,0 +1,22 @@
+package com.demo.catalog.tests;
+
+import java.time.Instant;
+
+import com.demo.catalog.dto.ProductDTO;
+import com.demo.catalog.models.Category;
+import com.demo.catalog.models.Product;
+
+public class Factory {
+	
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-21T04:00:00Z"));
+		product.getCategories().add(new Category(2L, "Electronics"));
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+	
+}
