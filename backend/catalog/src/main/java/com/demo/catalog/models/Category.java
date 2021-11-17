7fa39b8dd1fb0ad002b6c36e828c3d27f11bbcab
Alberto Nascimento <beto.bdn@gmail.com>
2021-11-17 09:49:35 -0300

    Add entities User and Role

--- backend/catalog/src/main/java/com/demo/catalog/models/Category.java
+++ backend/catalog/src/main/java/com/demo/catalog/models/Category.java
@@ -2,12 +2,15 @@ package com.demo.catalog.models;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -28,10 +31,13 @@ public class Category implements Serializable {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
 	
+	@ManyToMany(mappedBy = "categories")	
+	private Set<Product> products = new HashSet<>();
+	
 	public Category() {
 		
 	}
-
+		
 	public Category(Long id, String name) {
 		this.id = id;
 		this.name = name;
@@ -65,6 +71,10 @@ public class Category implements Serializable {
 	@PreUpdate
 	public void preUpdate() {
 		updatedAt = Instant.now();
+	}
+	
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
