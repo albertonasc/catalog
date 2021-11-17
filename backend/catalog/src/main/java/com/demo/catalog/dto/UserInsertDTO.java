edf6abc7014a3765cfedfcf0a6936fb046e16121
Alberto Nascimento <beto.bdn@gmail.com>
2021-11-17 15:23:22 -0300

    Custom ConstraintValidator

--- backend/catalog/src/main/java/com/demo/catalog/dto/UserInsertDTO.java
+++ backend/catalog/src/main/java/com/demo/catalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.demo.catalog.dto;
 
+import com.demo.catalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
